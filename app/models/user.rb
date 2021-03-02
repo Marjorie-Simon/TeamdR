f1362b7447eb8d4c75838d4a0d515c2117b5c61b
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-02 18:29:53 +0100

    setup cloudinary active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   has_one_attached :photo
   validates :first_name, presence: :true
   validates :last_name, presence: :true
-  validates :manager, presence: :true
+ # validates :manager, presence: :true
   validates :email, uniqueness: :true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require "open-uri"
+# file = URI.open("https://res.cloudinary.com/dai1inff0/image/upload/v1614696012/vxgtpr6kiznptp3mrcc0.jpg")
+ida = User.new(first_name: "Ida", last_name: "Blabla", email: "ida.blablatadta@gmail.com", password:"123456", manager: false)
+# ida.photo.attach(io:file, filename: 'ida.png', content_type:'image/png')
+ida.save!
