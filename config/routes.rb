ad680b4041c7bd42a7443442d964d462e30f59e8
diego boy <diegolive5000@gmail.com>
2021-03-02 19:29:50 +0100

    create all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: :index
+  resources :users, only: :index do
+    resources :events, only: [ :new, :create ]
+  resources :daily_statuses, only: [ :index, :create, :update ]
+  resources :events, only: [ :index, :approve, :decline ]
+  resources :ratings, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
