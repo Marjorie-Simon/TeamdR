7812e8baed4707b31747f2a2a8b71080805180c1
diego boy <diegolive5000@gmail.com>
2021-03-02 19:41:31 +0100

    add two missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, only: :index do
     resources :events, only: [ :new, :create ]
-  resources :daily_statuses, only: [ :index, :create, :update ]
+
+  resources :daily_statuses, only: [ :index, :create, :update, :update_all ]
+
   resources :events, only: [ :index, :approve, :decline ]
+
   resources :ratings, only: :create
+
+  resources :dashboard, only: :dashboard
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
