4d797f06af0f7b7af12a61c38c6cee5e7f0a2178
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-02 14:55:06 +0100

    fixed gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, :daily_statuses
+  has_many :events
+  has_many :daily_statuses
   has_many :ratings, through: :daily_statuses
   validates :first_name, presence: :true
   validates :last_name, presence: :true
