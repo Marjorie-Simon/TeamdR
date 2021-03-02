d2520863862dc903862d4f470c11a6910346659f
Friederike Spring <friederikespring@posteo.de>
2021-03-02 13:17:35 +0100

    create models

---
+++ app/models/event.rb
@@ -0,0 +1,8 @@
+class Event < ApplicationRecord
+  belongs_to :user
+  belongs_to :invitee, class_name: "User"
+  validates :title, presence: :true
+  validates :start_time, presence: :true
+  validates :end_time, presence: :true
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :events, :daily_statuses
+  has_many :ratings, through: :daily_statuses
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
+  validates :manager, presence: :true
+  validates :email, uniqueness: :true
 end
