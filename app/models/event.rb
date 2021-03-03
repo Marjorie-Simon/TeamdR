ef02cfc2bfbd5e88d90530f1552c4d07204048c4
Friederike Spring <friederikespring@posteo.de>
2021-03-03 11:25:56 +0100

    user seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :invitee, class_name: "User"
   validates :title, presence: :true
   validates :start_time, presence: :true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require "open-uri"
 # file = URI.open("https://res.cloudinary.com/dai1inff0/image/upload/v1614696012/vxgtpr6kiznptp3mrcc0.jpg")
+require 'Faker'
+
 puts "Cleaning database..."
 User.destroy_all
 
@@ -14,5 +16,9 @@ puts "Creating users..."
 ida = User.create(first_name: "Ida", last_name: "Blabla", email: "ida.blabla@zalando.com", password:"123456", manager: false)
 # ida.photo.attach(io:file, filename: 'ida.png', content_type:'image/png')
 angela = User.create(first_name: "Angela", last_name: "Meek", email: "angela.meek@zalando.com", password:"123456", manager: true)
+
+50.times do |user|
+  user = User.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, email: Faker::Internet.email, password:Faker::Number.number(digits: 6), manager: false)
+end
 
 puts "Finished!"
