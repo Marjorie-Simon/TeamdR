b83a3c7025baafa97de178f3175a07a43a9ab12f
Inbal Gordon <inbalgordon@gmail.com>
2021-03-03 12:16:03 +0100

    fixed routes for user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: :index do
+  resources :users do
     resources :events, only: [ :new, :create ]
   end
 
