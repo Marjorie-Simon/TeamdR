2017a34253ed2e6fb54e8cdeb4f680cb9e590968
Friederike Spring <friederikespring@posteo.de>
2021-03-03 18:26:55 +0100

    add controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < ApplicationController
+  def index
+    @events = Events.all
+  end
+
+  def new
+    @event = Event.new
+    authorize @restaurant
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content, :status, :user_id, :invitee_id, :start_time, :end_time)
+  end
+end
