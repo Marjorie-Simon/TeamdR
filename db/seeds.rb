b1caf9b904e2004d8e1b013dbbcffba11dc523ef
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-04 13:30:29 +0100

    added some seeds-does not work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,53 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require "open-uri"
 # file = URI.open("https://res.cloudinary.com/dai1inff0/image/upload/v1614696012/vxgtpr6kiznptp3mrcc0.jpg")
-require 'faker'
+#require 'faker'
+require 'json'
+require 'open-uri'
 
-puts "Cleaning database..."
-User.destroy_all
-
+# creating all users from google spreadheet
 puts "Creating users..."
-ida = User.create(first_name: "Ida", last_name: "Blabla", email: "ida.blabla@zalando.com", password:"123456", manager: false)
-# ida.photo.attach(io:file, filename: 'ida.png', content_type:'image/png')
-angela = User.create(first_name: "Angela", last_name: "Meek", email: "angela.meek@zalando.com", password:"123456", manager: true)
 
-50.times do |user|
-  user = User.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, email: Faker::Internet.email, password:Faker::Number.number(digits: 6), manager: false)
+
+user_url = 'https://spreadsheets.google.com/feeds/cells/1CW_PI1spEoZ-e87TD9TvwVBz1-n2etWw5wGufLOC9I0/1/public/full?alt=json'
+user_seed_url = open(user_url).read
+user_seed_json = JSON.parse(user_seed_url)
+
+user_seed_json['feed']['entry'].each do |seed|
+  user = User.new(
+    first_name:  seed['gsx$firstname']['$t'],
+    last_name: seed['gsx$lastname']['$t'],
+    email: seed ['gsx$email']['$t'],
+    password: seed ['gsx$password']['$t'],
+    manager: seed ['gsx$manager']['$t'],
+    photo: "https://res.cloudinary.com/dai1inff0/image/upload/v1614858775/users/photo/#{seed['gsx$photo']['$t']}.jpg"
+  )
+  user.save!
 end
 
-puts "Creating statuses..."
-title = [ "in office", "business trip", "home office", "vacation", "sick leave"]
-status_ida = DailyStatus.create(title: "in office", date: 2021-02-12, user_id: 1)
+puts "Successfully created users. Easy!"
 
-puts "Finished!"
+
+
+
+
+
+
+
+#puts "Cleaning database..."
+#User.destroy_all
+
+#puts "Creating users..."
+#ida = User.create(first_name: "Ida", last_name: "Blabla", email: "ida.blabla@zalando.com", password:"123456", manager: false)
+# ida.photo.attach(io:file, filename: 'ida.png', content_type:'image/png')
+#angela = User.create(first_name: "Angela", last_name: "Meek", email: "angela.meek@zalando.com", password:"123456", manager: true)
+
+#50.times do |user|
+#  user = User.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, email: Faker::Internet.email, password:Faker::Number.number(digits: 6), manager: false)
+#end
+
+#puts "Creating statuses..."
+#title = [ "in office", "business trip", "home office", "vacation", "sick leave"]
+#status_ida = DailyStatus.create(title: "in office", date: 2021-02-12, user_id: 1)
+
+#
