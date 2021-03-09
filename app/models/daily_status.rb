5e0aabddbbbb5089311265d9d6e3d00e052302c3
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-09 15:53:36 +0100

    finally fixed our simple calendar

--- app/models/daily_status.rb
+++ app/models/daily_status.rb
@@ -1,8 +1,8 @@
 class DailyStatus < ApplicationRecord
-  extend SimpleCalendar
+ # extend SimpleCalendar
   belongs_to :user
   has_one :rating, dependent: :destroy
   validates :date, presence: :true
 
-  has_calendar :attribute => :date
+# has_calendar :attribute => :date
 end
