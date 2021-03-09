ce7ac5b94289eafbc6de2e30f660d520dee7dcfd
diego boy <diegolive5000@gmail.com>
2021-03-09 09:17:22 +0100

    add numericality and less_than to rating validation

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,4 @@
 class Rating < ApplicationRecord
   belongs_to :daily_status
-  validates :value_stars, presence: :true
+  validates :value_stars, presence: :true, numericality: { only_integer: true, less_than: 6 }
 end
