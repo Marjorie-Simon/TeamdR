674e34eb5939514a22db2f8fbf122bfb6496e169
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-09 09:29:51 +0100

    ni change just attempts

--- app/models/daily_status.rb
+++ app/models/daily_status.rb
@@ -2,6 +2,8 @@ class DailyStatus < ApplicationRecord
   belongs_to :user
   has_many :ratings
   validates :date, presence: :true
+  validates :title, uniqueness: { scope: :date,
+    message: "you can only have one status for a given day" }
 
   extend SimpleCalendar
   has_calendar :attribute => :date
