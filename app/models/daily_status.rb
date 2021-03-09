b64d4b86244cc1aab81354ca0ca4757f81154b28
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-09 09:42:34 +0100

    fixed again

--- app/models/daily_status.rb
+++ app/models/daily_status.rb
@@ -3,8 +3,7 @@ class DailyStatus < ApplicationRecord
   belongs_to :user
   has_one :rating, dependent: :destroy
   validates :date, presence: :true
-  validates :title, uniqueness: { scope: :date,
-    message: "you can only have one status for a given day" }
+
 
   has_calendar :attribute => :date
 end
