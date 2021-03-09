bb0864dc9299156945542d3d7c5e47a27283b53e
Marjorie Simon <marjoriesimon13@gmail.com>
2021-03-09 13:04:54 +0100

    create update button

--- app/controllers/daily_statuses_controller.rb
+++ app/controllers/daily_statuses_controller.rb
@@ -4,14 +4,23 @@ class DailyStatusesController < ApplicationController
     @daily_status.user = current_user
     authorize @daily_status
     if @daily_status.save
-      redirect_to root_path(date: @daily_status.date), notice: "You have updated your status for #{@daily_status.date}"
+      redirect_to root_path(date: @daily_status.date), notice: "You have created your status for #{@daily_status.date}"
     else
       render :index
     end
 
   end
 
-
+  def update
+    @daily_status = current_user.daily_statuses.find_by(date: params[:daily_status][:date])
+    @daily_status.update(daily_status_params)
+     authorize @daily_status
+      if @daily_status.save
+          redirect_to root_path(date: @daily_status.date), notice: "You have updated your status for #{@daily_status.date}"
+      else
+      render :index
+    end
+  end
 
 
 
